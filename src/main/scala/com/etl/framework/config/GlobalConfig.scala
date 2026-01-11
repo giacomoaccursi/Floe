2074c2b5447802375bed70bf3a5d7e5e9b03a08b
giacomo accursi <giacomo.accursi@gmail.com>
2026-01-11 14:20:00 +0100

    feat: create GlobalConfig

---
+++ src/main/scala/com/etl/framework/config/GlobalConfig.scala
@@ -0,0 +1,59 @@
+package com.etl.framework.config
+
+/**
+ * Global configuration for the ETL framework
+ */
+case class GlobalConfig(
+                         spark: SparkConfig,
+                         paths: PathsConfig,
+                         processing: ProcessingConfig,
+                         monitoring: MonitoringConfig,
+                       )
+
+/**
+ * Spark configuration
+ */
+case class SparkConfig(
+                        appName: String,
+                        master: String,
+                        config: Map[String, String]
+                      )
+
+/**
+ * Path configuration for input/output directories
+ */
+case class PathsConfig(
+                        inputBase: String,
+                        outputBase: String,
+                        validatedPath: String,
+                        rejectedPath: String,
+                        metadataPath: String,
+                        modelPath: String,
+                        stagingPath: String,
+                        checkpointPath: String
+                      )
+
+/**
+ * Processing configuration
+ */
+case class ProcessingConfig(
+                             batchIdFormat: String,
+                             executionMode: String,
+                             failOnValidationError: Boolean,
+                             maxRejectionRate: Double,
+                             rollbackOnFailure: Boolean,
+                             checkpointEnabled: Boolean,
+                             checkpointInterval: String
+                           )
+
+
+/**
+ * Monitoring configuration
+ */
+case class MonitoringConfig(
+                             enabled: Boolean,
+                             metricsExporter: Option[String],
+                             metricsEndpoint: Option[String],
+                             logLevel: String
+                           )
+

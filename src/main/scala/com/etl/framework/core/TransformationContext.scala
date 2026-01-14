ac679c535f135c60d1ca1c739485182a11a3186f
giacomo accursi <giacomo.accursi@gmail.com>
2026-01-14 22:20:00 +0100

    feat: implement TransformationContext

--- src/main/scala/com/etl/framework/core/TransformationContext.scala
+++ src/main/scala/com/etl/framework/core/TransformationContext.scala
@@ -6,9 +6,49 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
  * Context available during transformations (both pre- and post-validation)
  */
 case class TransformationContext(
-                                  currentFlow: String,
-                                  currentData: DataFrame,
-                                  validatedFlows: Map[String, DataFrame],
-                                  batchId: String,
-                                  spark: SparkSession
-                                )
+                                  currentFlow: String,                           // Current flow name
+                                  currentData: DataFrame,                        // Current flow data
+                                  validatedFlows: Map[String, DataFrame],        // All already validated flows
+                                  batchId: String,                               // Current batch ID
+                                  spark: SparkSession                            // Spark session
+                                ) {
+
+  /**
+   * Get DataFrame of another already validated flow
+   */
+  def getFlow(flowName: String): Option[DataFrame] = {
+    validatedFlows.get(flowName)
+  }
+
+  /**
+   * Add a new table that will be automatically saved
+   * and made available for Transformation (DAG aggregation)
+   */
+  def addTable(
+                tableName: String,
+                data: DataFrame,
+                outputPath: Option[String] = None,
+                dagMetadata: Option[AdditionalTableMetadata] = None
+              ): Unit = {
+    // Table will be automatically saved by framework
+    // and made available for Transformation if dagMetadata is specified
+    // Implementation will be handled by FlowExecutor
+  }
+}
+
+/**
+ * Metadata for integrating additional tables into DAG
+ */
+case class AdditionalTableMetadata(
+                                    primaryKey: Seq[String],
+                                    joinKeys: Map[String, Seq[String]] = Map.empty,
+                                    description: Option[String] = None,
+                                    partitionBy: Seq[String] = Seq.empty
+                                  )
+
+/**
+ * Function type for transformations
+ */
+object FlowTransformation {
+  type FlowTransformation = TransformationContext => DataFrame
+}

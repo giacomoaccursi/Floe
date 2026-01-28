86144f2b7089ed360e08ce276f2535155840731f
giacomo accursi <giacomo.accursi@gmail.com>
2026-01-28 23:39:04 +0100

    feat: add timeutil that return result and duration

--- src/main/scala/com/etl/framework/util/TimingUtil.scala
+++ src/main/scala/com/etl/framework/util/TimingUtil.scala
@@ -24,4 +24,27 @@ object TimingUtil {
         throw e
     }
   }
+  
+  /**
+   * Execute operation with automatic timing and logging, returning both result and duration
+   * 
+   * @param logger Logger instance for logging
+   * @param operation Description of the operation being timed
+   * @param block Code block to execute and time
+   * @return Tuple of (result, durationMs)
+   */
+  def timedWithDuration[T](logger: Logger, operation: String)(block: => T): (T, Long) = {
+    val startTime = System.nanoTime()
+    try {
+      val result = block
+      val durationMs = (System.nanoTime() - startTime) / 1000000
+      logger.info(s"$operation completed in ${durationMs}ms")
+      (result, durationMs)
+    } catch {
+      case e: Throwable =>
+        val durationMs = (System.nanoTime() - startTime) / 1000000
+        logger.error(s"$operation failed after ${durationMs}ms: ${e.getMessage}", e)
+        throw e
+    }
+  }
 }

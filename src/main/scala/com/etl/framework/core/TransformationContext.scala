d61f68a1cbf476db4e8dfc6976aa87bffd1966b7
giacomo accursi <giacomo.accursi@gmail.com>
2026-02-04 00:43:22 +0100

    refactor: make TransformationContext immutable

--- src/main/scala/com/etl/framework/core/TransformationContext.scala
+++ src/main/scala/com/etl/framework/core/TransformationContext.scala
@@ -2,55 +2,78 @@ package com.etl.framework.core
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-import scala.collection.mutable
-
 /**
- * Context available during transformations (both pre- and post-validation)
+ * Immutable context available during transformations (both pre- and post-validation)
  */
-case class TransformationContext(
-                                  currentFlow: String,                           // Current flow name
-                                  currentData: DataFrame,                        // Current flow data
-                                  validatedFlows: Map[String, DataFrame],        // All already validated flows
-                                  batchId: String,                               // Current batch ID
-                                  spark: SparkSession,                           // Spark session
-                                  private val additionalTablesMap: mutable.Map[String, AdditionalTableInfo] = mutable.Map.empty
-                                ) {
+final case class TransformationContext private(
+  currentFlow: String,
+  currentData: DataFrame,
+  validatedFlows: Map[String, DataFrame],
+  batchId: String,
+  spark: SparkSession,
+  additionalTables: Map[String, AdditionalTableInfo]
+) {
 
   /**
    * Get DataFrame of another already validated flow
    */
-  def getFlow(flowName: String): Option[DataFrame] = {
+  def getFlow(flowName: String): Option[DataFrame] = 
     validatedFlows.get(flowName)
-  }
 
   /**
-   * Add a new table that will be automatically saved
-   * and made available for Transformation (DAG aggregation)
+   * Add a new table and return a new context.
+   * The table will be automatically saved and made available for DAG aggregation.
    */
   def addTable(
-                tableName: String,
-                data: DataFrame,
-                outputPath: Option[String] = None,
-                dagMetadata: Option[AdditionalTableMetadata] = None
-              ): Unit = {
-    additionalTablesMap(tableName) = AdditionalTableInfo(
+    tableName: String,
+    data: DataFrame,
+    outputPath: Option[String] = None,
+    dagMetadata: Option[AdditionalTableMetadata] = None
+  ): TransformationContext = {
+    val tableInfo = AdditionalTableInfo(
       tableName = tableName,
       data = data,
       outputPath = outputPath,
       dagMetadata = dagMetadata
     )
+    copy(additionalTables = additionalTables + (tableName -> tableInfo))
   }
+  
+  /**
+   * Get all additional tables that were added during transformation
+   */
+  def getAdditionalTables: Map[String, AdditionalTableInfo] = additionalTables
+}
+
+object TransformationContext {
+  /**
+   * Create a new transformation context
+   */
+  def apply(
+    currentFlow: String,
+    currentData: DataFrame,
+    validatedFlows: Map[String, DataFrame],
+    batchId: String,
+    spark: SparkSession
+  ): TransformationContext = new TransformationContext(
+    currentFlow = currentFlow,
+    currentData = currentData,
+    validatedFlows = validatedFlows,
+    batchId = batchId,
+    spark = spark,
+    additionalTables = Map.empty
+  )
 }
 
 /**
  * Metadata for integrating additional tables into DAG
  */
 case class AdditionalTableMetadata(
-                                    primaryKey: Seq[String],
-                                    joinKeys: Map[String, Seq[String]] = Map.empty,
-                                    description: Option[String] = None,
-                                    partitionBy: Seq[String] = Seq.empty
-                                  )
+  primaryKey: Seq[String],
+  joinKeys: Map[String, Seq[String]] = Map.empty,
+  description: Option[String] = None,
+  partitionBy: Seq[String] = Seq.empty
+)
 
 /**
  * Information about an additional table created during transformations

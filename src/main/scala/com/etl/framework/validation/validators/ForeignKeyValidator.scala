051e45afde5647ef34461f59d94ba58f415c2b19
giacomo accursi <giacomo.accursi@gmail.com>
2026-02-04 00:41:55 +0100

    refactor: replace var with foldLeft in ForeignKeyValidator

--- src/main/scala/com/etl/framework/validation/validators/ForeignKeyValidator.scala
+++ src/main/scala/com/etl/framework/validation/validators/ForeignKeyValidator.scala
@@ -20,39 +20,39 @@ class ForeignKeyValidator(
       return ValidationUtils.validResult(df)
     }
     
-    var currentDf = df
-    var rejectedDf: Option[DataFrame] = None
-    
-    for (fk <- flowConfig.validation.foreignKeys) {
-      validatedFlows.get(fk.references.flow) match {
-        case None =>
-          throw ValidationConfigException(
-            s"Referenced flow '${fk.references.flow}' not found in flow $flowName"
-          )
-        
-        case Some(referencedFlow) =>
-          // Perform left anti join to find orphan records
-          val orphans = currentDf
-            .join(
-              referencedFlow.select(fk.references.column),
-              currentDf(fk.column) === referencedFlow(fk.references.column),
-              "left_anti"
+    flowConfig.validation.foreignKeys.foldLeft(ValidationStepResult(df, None)) {
+      case (ValidationStepResult(currentDf, rejectedAcc), fk) =>
+        validatedFlows.get(fk.references.flow) match {
+          case None =>
+            throw ValidationConfigException(
+              s"Referenced flow '${fk.references.flow}' not found in flow $flowName"
             )
           
-          if (!orphans.isEmpty) {
-            val orphansWithMetadata = ValidationUtils.addRejectionMetadata(
-              orphans,
-              "FK_VIOLATION",
-              s"Foreign key violation in flow $flowName: ${fk.name} (${fk.column} -> ${fk.references.flow}.${fk.references.column})",
-              "fk_validation"
-            )
+          case Some(referencedFlow) =>
+            // Perform left anti join to find orphan records
+            val orphans = currentDf
+              .join(
+                referencedFlow.select(fk.references.column),
+                currentDf(fk.column) === referencedFlow(fk.references.column),
+                "left_anti"
+              )
             
-            rejectedDf = ValidationUtils.combineRejected(rejectedDf, Some(orphansWithMetadata))
-            currentDf = currentDf.join(orphans.select(fk.column), Seq(fk.column), "left_anti")
-          }
-      }
+            if (orphans.isEmpty) {
+              ValidationStepResult(currentDf, rejectedAcc)
+            } else {
+              val orphansWithMetadata = ValidationUtils.addRejectionMetadata(
+                orphans,
+                "FK_VIOLATION",
+                s"Foreign key violation in flow $flowName: ${fk.name} (${fk.column} -> ${fk.references.flow}.${fk.references.column})",
+                "fk_validation"
+              )
+              
+              val newRejected = ValidationUtils.combineRejected(rejectedAcc, Some(orphansWithMetadata))
+              val newCurrent = currentDf.join(orphans.select(fk.column), Seq(fk.column), "left_anti")
+              
+              ValidationStepResult(newCurrent, newRejected)
+            }
+        }
     }
-    
-    ValidationStepResult(currentDf, rejectedDf)
   }
 }

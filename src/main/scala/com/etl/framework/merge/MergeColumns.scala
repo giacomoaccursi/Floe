f140e7390a51346f005cd7a155a989e579600cd5
giacomo accursi <giacomo.accursi@gmail.com>
2026-03-28 15:01:04 +0100

    chore: remove dead merge module and stale docs

--- src/main/scala/com/etl/framework/merge/MergeColumns.scala
+++
@@ -1,9 +0,0 @@
-package com.etl.framework.merge
-
-/**
- * Column names used internally by merge operations
- */
-private[merge] object MergeColumns {
-  val ROW_NUM = "_row_num"
-  val COUNT = "_count"
-}
